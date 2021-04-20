0a183a2d6fa9b5e7a0f08ca7af7e9a10b15c68ec
Cannon <teamnapinfo@gmail.com>
2021-04-20 13:08:13 +0100

    - lib folder

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Command, { CommandOptions } from "./Command";
-import CDHandler from "./CDHandler";
-export { Command, CDHandler };
-export type { CommandOptions };
-//# sourceMappingURL=index.d.ts.map
