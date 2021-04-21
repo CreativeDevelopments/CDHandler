03ad3b17c4b9cb690d7204bfc27b462cb4742ffb
Cannon <teamnapinfo@gmail.com>
2021-04-21 19:18:35 +0100

    || not &&

--- src/events/message.ts
+++ src/events/message.ts
@@ -145,7 +145,7 @@ export default (handler: any, client: Client, defaultPrefix: string, ping: boole
             let running = cmd.run ? cmd.run : cmd.fire ? cmd.fire : cmd.execute ? cmd.execute : cmd.callback ? cmd.callback : null
             if (running == null) throw new Error(colour("[CDHANDLER] [ERROR]", { textColour: "red" }) + " Missing run function in " + cmd.name);
             else r = await running({ message, args, client, handler })
-            if (typeof running == "string" && typeof running == "number") result = [running]
+            if (typeof running == "string" || typeof running == "number") result = [running]
             else result = [...running]
 
             if (result && result[0]) {
